363a4891fcf3830c190db2affafc0bca5f34c440
fossj117 <fossj117@gmail.com>
2022-02-22 20:08:51 -0500

    Fix strange mapbox transpiling bug

--- src/FloodMap.js
+++ src/FloodMap.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import Map from "react-map-gl"
 import { DeckGL } from 'deck.gl'
+import mapboxgl from 'mapbox-gl';
+// eslint-disable-next-line import/no-webpack-loader-syntax
+import MapboxWorker from 'worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker'; // Load worker code separately with worker-loader
+
+// from https://stackoverflow.com/questions/65434964/mapbox-blank-map-react-map-gl-reactjs
+mapboxgl.workerClass = MapboxWorker;
 
 export default function FloodMap() {
     const mapboxToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
